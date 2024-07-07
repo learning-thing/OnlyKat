28ec21edcd51c68fc9c627758c7ce18604904302
Tinolm <OMG-ichfind-keinen-namen@protonmail.com>
2024-07-07 17:20:53 +0200

    content

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 var x = 1;
 var clickCount = 0;
 
-function initmethod() {
+function ClickMeFunc() {
     clickCount++;
-    document.getElementById("clickme").innerHTML = "Click Me " + clickCount;
+    if (clickCount == 1) {
+        document.getElementById("clickme").innerHTML = "Clicked once";;
+    } else {
+        if (clickCount == 2) {
+            document.getElementById("clickme").innerHTML = "Clicked twice";
+        }
+    }
+    if (clickCount > 2) {
+        document.getElementById("clickme").innerHTML = "Clicked " + clickCount+ " times";
+    }
 
     if (x < 9) {
         x+=1;
-        console.log("X is zero");
-        document.getElementById("title").innerHTML = "Kat Level: "+(x+1);
-        document.getElementById("title").style.marginLeft = ((x+1)*20) + "px";
     } else {
         x = 0;
+    }
+    console.log("X is zero");
+    document.getElementById("title").innerHTML = "Kat Level: "+(clickCount+1);
+    document.getElementById("title").style.marginLeft = ((x+1)*20) + "px";
+
+    document.getElementById("clickme").style.marginLeft = ((x+1)*20) + "px";
+    document.getElementById("progbar").innerHTML = "Progress: " + (clickCount) + "%";
+    document.getElementById("progbar").style.width = (clickCount) + "%";
+    if (clickCount == 9) {
+        winner();
     }
 }
 
 
+function winner() {
+    alert("Winner");
+}
 
 //#initmethod();
