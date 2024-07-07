65aba82dfddeb045aab0bafbd01cefd3510eff1b
Tinolm <OMG-ichfind-keinen-namen@protonmail.com>
2024-07-07 18:38:39 +0200

    QOL improvements

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-var x = Number(document.cookie.valueOf("clicks").split("=")[1]);
-var clickCount = x;
+if (document.cookie.length > 1) {
+    var x = Number(document.cookie.valueOf("clicks").split("=")[1])-1;
+    var clickCount = x;
+} else {
+    var x = 0;
+    var clickCount = 0;
+}
 
 console.log(document.cookie.valueOf("clicks"));
 console.log(Number(document.cookie.valueOf("clicks").split("=")[1]));
 console.log(x);
+
+document.getElementById("LastKat").innerHTML = "Last Kat level: " + (clickCount);
 
 function ClickMeFunc() {
     clickCount++;
@@ -30,18 +37,37 @@ function ClickMeFunc() {
     document.getElementById("progbar").innerHTML = "Progress: " + (clickCount) + "%";
     
     if (clickCount > 12) {
-        document.getElementById("progbar").style.width = (12+clickCount) + "%";
+        document.getElementById("progbar").style.width = (clickCount) + "%";
     }
     if (clickCount == 100) {
-        document.cookie = "clicks=10;";
         winner();
     }
+
 }
 
+window.onbeforeunload = closingCode;
+function closingCode(){
+document.cookie = "clicks="+(clickCount+1)+";";
+   return null;
+}
 
 function winner() {
     alert("You're a Winner!");
     console.log("Winner!");
+}
+
+function ResetClicks() {
+    clickCount = 0;
+    x=0;
+    document.cookie = "clicks="+clickCount+";";
+    document.getElementById("clickme").innerHTML = "Click Me";
+    document.getElementById("progbar").style.width = "12%";
+    document.getElementById("progbar").innerHTML = "Progress: 0%";
+    document.getElementById("title").innerHTML = "Kat Level: 1";
+    document.getElementById("title").style.marginLeft = "0px";
+    document.getElementById("clickme").style.marginLeft = "0px";
+    document.getElementById("LastKat").innerHTML = "Last Kat level: 0";
+
 }
 
 //#initmethod();
